6af6e6a156b6f821315c8c913fba6d39344c47a6
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-07-14 17:42:42 -0500

    feat: agregar archivos creacion y manejo de grafos

--- src/dag.py
+++ src/dag.py
@@ -1,23 +1,21 @@
-
 import graphviz
 
-#funcion que genera earchiv dot
+
+# funcion que genera earchiv dot
 def leer_archivo_dot(ruta_dot):
-    # leer archivo .dot a digraph retorna un dag 
+    # leer archivo .dot a digraph retorna un dag
     try:
         with open(ruta_dot) as f:
             dot_grafo = f.read()
             grafo = graphviz.Source(dot_grafo)
             return grafo
-    #error en caso no pueda leer archivo
+    # error en caso no pueda leer archivo
     except Exception as e:
         print(f"error archivo DOT: {e}")
         return None
 
 
-
-
-#calcula la densidad de un nodo
+# calcula la densidad de un nodo
 def calcular_densidad(grafo, nodo):
     # calcula dependencia de un nodo en el grafo
     if grafo is None:
@@ -33,7 +31,7 @@ def calcular_densidad(grafo, nodo):
         return None
 
 
-#dikstra inverso halla camino desde Head hasta un nodo u
+# dikstra inverso halla camino desde Head hasta un nodo u
 def dikstra_inverso(grafo, nodo):
     # calcula camino desde heaad hasta un nodo u
     if grafo is None:
@@ -42,12 +40,12 @@ def dikstra_inverso(grafo, nodo):
         camino = []
         visitados = set()
         pila = [(nodo, [nodo])]
-        #apila nodos y recorre el grafo
+        # apila nodos y recorre el grafo
         while pila:
             actual, ruta = pila.pop()
             if actual not in visitados:
                 visitados.add(actual)
-                #caundo llegue a head guarda el camino
+                # caundo llegue a head guarda el camino
                 if actual == 'HEAD':
                     camino = ruta[::-1]
                     break
@@ -59,9 +57,10 @@ def dikstra_inverso(grafo, nodo):
         print(f"error : {e}")
         return None
 
-#halla los k commits con mayor indegree
-#para esto se halla todos los caminos y se ordena por indegree y retorrna los k mayores
-def k_commits_mayor_indegree(grafo, k): 
+
+# halla los k commits con mayor indegree
+# para esto se halla todos los caminos y se ordena por indegree y retorrna los k mayores
+def k_commits_mayor_indegree(grafo, k):
     # calcula los k commits con mayor indegree
     if grafo is None:
         return None
@@ -77,4 +76,3 @@ def k_commits_mayor_indegree(grafo, k):
     except Exception as e:
         print(f"error : {e}")
         return None
-
