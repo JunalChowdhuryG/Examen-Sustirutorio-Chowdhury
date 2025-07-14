27f169575f98019fd46d5cf433818aa86fd9dc8d
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-07-14 17:33:26 -0500

    feat: agregar archivos creacion y manejo de grafos

---
+++ src/dag.py
@@ -0,0 +1,80 @@
+
+import graphviz
+
+#funcion que genera earchiv dot
+def leer_archivo_dot(ruta_dot):
+    # leer archivo .dot a digraph retorna un dag 
+    try:
+        with open(ruta_dot) as f:
+            dot_grafo = f.read()
+            grafo = graphviz.Source(dot_grafo)
+            return grafo
+    #error en caso no pueda leer archivo
+    except Exception as e:
+        print(f"error archivo DOT: {e}")
+        return None
+
+
+
+
+#calcula la densidad de un nodo
+def calcular_densidad(grafo, nodo):
+    # calcula dependencia de un nodo en el grafo
+    if grafo is None:
+        return None
+    try:
+        dependencias = []
+        for edge in grafo.edges:
+            if edge[1] == nodo:
+                dependencias.append(edge[0])
+        return len(dependencias)
+    except Exception as e:
+        print(f"error : {e}")
+        return None
+
+
+#dikstra inverso halla camino desde Head hasta un nodo u
+def dikstra_inverso(grafo, nodo):
+    # calcula camino desde heaad hasta un nodo u
+    if grafo is None:
+        return None
+    try:
+        camino = []
+        visitados = set()
+        pila = [(nodo, [nodo])]
+        #apila nodos y recorre el grafo
+        while pila:
+            actual, ruta = pila.pop()
+            if actual not in visitados:
+                visitados.add(actual)
+                #caundo llegue a head guarda el camino
+                if actual == 'HEAD':
+                    camino = ruta[::-1]
+                    break
+                for edge in grafo.edges:
+                    if edge[1] == actual:
+                        pila.append((edge[0], ruta + [edge[0]]))
+        return camino
+    except Exception as e:
+        print(f"error : {e}")
+        return None
+
+#halla los k commits con mayor indegree
+#para esto se halla todos los caminos y se ordena por indegree y retorrna los k mayores
+def k_commits_mayor_indegree(grafo, k): 
+    # calcula los k commits con mayor indegree
+    if grafo is None:
+        return None
+    try:
+        # creamos diccionario
+        indegree = {}
+        for edge in grafo.edges:
+            if edge[1] not in indegree:
+                indegree[edge[1]] = 0
+            indegree[edge[1]] += 1
+        # ordena por indegree retorna los k mayores
+        return sorted(indegree.items(), key=lambda x: x[1], reverse=True)[:k]
+    except Exception as e:
+        print(f"error : {e}")
+        return None
+
---
+++ src/git_graph.py
@@ -0,0 +1,19 @@
+import subprocess
+import graphviz 
+
+
+def generar_archivo_dot():
+    #genera archivo .dot
+    subprocess.run(
+                ['git', 'graph','-c','-p','./demo']
+            )
+
+    #carga el archivo .dot y lo convierte a imagen
+    with open('demo/git_graph.dot') as f:
+        dot_graph = f.read()
+        graph = graphviz.Source(dot_graph, format='png')
+        graph.render('demo/git_graph.png', cleanup=True)  # guarda imagen y limpia archivos temporales
+        graphviz.Source.from_file('demo/git_graph.dot')
+
+if __name__ == "__main__":
+    generar_archivo_dot()
